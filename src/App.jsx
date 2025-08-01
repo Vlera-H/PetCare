441538ee55aa838c2e279d00382586210f989a21
Vlera-H <vlerahaxhaj@gmail.com>
2025-08-01 12:41:01 +0200

    ndryshime ne paketa

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Hero from './components/Hero';
 const App = () => {
     return (
         <div className='w-full max-w-[1440px] mx-auto bg-orange-quaternary relative'>
+        
+        
+            <div className="bg-orange-tertiary text-white p-4 text-xl mb-4">
+                TailwindCSS po funksionon 🎉
+            </div>
+
             <Hero />
         </div>
     );

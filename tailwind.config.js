567b83c483303ad52d32d4692921cc5e5b23742f
Vlera-H <vlerahaxhaj@gmail.com>
2025-08-05 19:24:27 +0200

    UPDATES

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-// /** @type {import('tailwindcss').Config} */
+// // /** @type {import('tailwindcss').Config} */
 // export default {
 //   content: [
 //     "./index.html",
@@ -18,10 +18,8 @@
 //   plugins: [],
 // }
 /**
- * 
- *//*module.exports e pata */
 
-  /**/
+//   /**/
   
   /** @type {import('tailwindcss').Config} */
 export default {
